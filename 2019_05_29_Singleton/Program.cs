b627a7f93cd79c03b274b11a4ccac26d60f26e82
Steven Shao <stevensh@microsoft.com>
2019-05-29 17:35:34 +0800

    Add project files.

---
+++ 2019_05_29_Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SingletonLib;
+
+namespace _2019_05_29_Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton5 xxx = Singleton5.Instance;
+        }
+    }
+}
---
+++ SingletonLib/Singleton5.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonLib
+{
+    public sealed class Singleton5 // The key word "sealed" here prevents inheritance.
+    {
+        public void doSomeWork()
+        {
+            Console.WriteLine("Singleton5 is working.");
+        }
+
+        private Singleton5() // private prevents instantiation via constructor directly outside this class.
+        {
+            // We can do some init work here.
+            // For example, load config etc.
+
+        }
+        public static Singleton5 Instance // define a property to get the Singleton instance.
+        {
+            get
+            {
+                return Nested.instance;
+            }
+        }
+
+        class Nested // By default, it's private.
+        {
+            static Nested()
+            {
+                // We can also do some one time init work here;
+
+            }
+
+            internal static readonly Singleton5 instance = new Singleton5();
+        }
+
+    }
+}
